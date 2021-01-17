3eb487f00fce038146adc7984012b775090e5a3c
ji_0kim <jiyoung26091212@gmail.com>
2021-01-17 23:21:14 +0900

    Add components features

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-      footer
+  <div class="clearAllContainer">
+      <span class="clearAllBtn" v-on:click="clearTodo">
+          Clear All
+      </span>
   </div>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        clearTodo: function (){
+            localStorage.clear();
+        }, 
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+    .clearAllContainer {display: block; width: 8.5rem; height: 50px; line-height: 50px; background: #fff; border-radius: 5px; margin: 0 auto; cursor: pointer;}
+    .clearAllBtn {color: #e20303; display: block;}
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,17 @@
 <template>
     <div>
         <ul>
-            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-                {{ todoItem }}
+            <li v-for="(todoItem, index) in todoItems" 
+                v-bind:key="todoItem.item" class="shadow">
+                <span class="checkBtn" 
+                    v-on:click="toggleComplete(todoItem, index)" 
+                    v-bind:class="{checkBtnCompleted: todoItem.completed}">
+                    <i class="fas fa-check">
+                    </i>
+                </span>
+                <span v-bind:class="{textCompleted: todoItem.completed}">
+                    {{ todoItem.item }}
+                </span>
                 <span class="remove-btn" v-on:click="removeTodo(todoItem, index)">
                     <i class="fas fa-trash-alt"></i>
                 </span>
@@ -10,39 +19,43 @@
         </ul>
     </div>
 </template>
-
+ 
 <script>
 export default {
     data: function(){
         return {
             todoItems: []
-            
         }
     },
     methods: {
         removeTodo: function(todoItem, index) {
             localStorage.removeItem(todoItem, index);  
             this.todoItems.splice(index, 1);
+        },
+        toggleComplete: function (todoItem, index) {
+            todoItem.completed = !todoItem.completed;
+            localStorage.removeItem(todoItem.item);
+            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+            // console.log(!todoItem.completed);
         }
     },
     created: function(){
         if(localStorage.length > 0){
             for (var i = 0; i < localStorage.length; i ++){
                 if (localStorage.key(i) !=='loglevel:webpack-dev-server'){
-                    this.todoItems.push(localStorage.key(i));
+                    this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
                 }
             }
         }
-        
     }
 }
 </script>
 
-<style>
+<style scoped>
 ul {list-style-type: none; padding-left: 0; margin-top: 0; text-align: left;}
 li {display: flex; min-height: 50px; height: 50px; line-height: 50px; margin: 0.5rem 0; padding: 0 0.9rem; background: #fff; border-radius: 5px;}
 .remove-btn {margin-left: auto; color: #de4343; cursor: pointer;}
-.check-btn {line-height: 45px; color: #62acde; margin-right: 5px;}
-.check-btn-completed {color: #b3adad;}
-.text-completed {text-decoration: line-through; color: #b3adad;}
+.checkBtn {line-height: 45px; color: #62acde; margin-right: 5px;}
+.checkBtnCompleted {color: #b3adad;}
+.textCompleted {text-decoration: line-through; color: #b3adad;}
 </style>
