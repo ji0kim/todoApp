27014c0b23c69ed2171f8a292c632e79d9dbc6f6
ji_0kim <jiyoung26091212@gmail.com>
2021-01-17 19:12:06 +0900

    -components setting -Add create, delete feature

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="input-box shadow">
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+      <span class="add-container" v-on:click="addTodo"><i class="fas fa-plus add-btn"></i></span>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function (){
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.clearInput();
+        },
+        clearInput: function(){
+            this.newTodoItem = ''; 
+        }
+    }
+}
+</script>
+
+<style >
+.input-box {background: white; height: 50px; line-height: 50px; border-radius: 5px;}
+.input-box input {border-style: none; font-size: 0.9rem; width: calc(100% - 80px); line-height: 48px;}
+.add-container {float: right; background: linear-gradient(to right, #6478FB, #8763FB); display: block; width: 3rem; border-radius: 0 5px 5px 0;}
+.add-btn {color: #fff; vertical-align: middle;}
+</style>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
+                {{ todoItem }}
+                <span class="remove-btn" v-on:click="removeTodo(todoItem, index)">
+                    <i class="fas fa-trash-alt"></i>
+                </span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            todoItems: []
+            
+        }
+    },
+    methods: {
+        removeTodo: function(todoItem, index) {
+            localStorage.removeItem(todoItem, index);  
+            this.todoItems.splice(index, 1);
+        }
+    },
+    created: function(){
+        if(localStorage.length > 0){
+            for (var i = 0; i < localStorage.length; i ++){
+                if (localStorage.key(i) !=='loglevel:webpack-dev-server'){
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+        
+    }
+}
+</script>
+
+<style>
+ul {list-style-type: none; padding-left: 0; margin-top: 0; text-align: left;}
+li {display: flex; min-height: 50px; height: 50px; line-height: 50px; margin: 0.5rem 0; padding: 0 0.9rem; background: #fff; border-radius: 5px;}
+.remove-btn {margin-left: auto; color: #de4343; cursor: pointer;}
+.check-btn {line-height: 45px; color: #62acde; margin-right: 5px;}
+.check-btn-completed {color: #b3adad;}
+.text-completed {text-decoration: line-through; color: #b3adad;}
+</style>
